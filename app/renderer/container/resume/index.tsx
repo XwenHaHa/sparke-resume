182e307b54f41f8d6f04abb7c39b124d5c5cbcbf
XwenHaHa <929716663@qq.com>
2024-07-24 11:42:45 +0800

    feat:新增SparkeUpload组件

--- app/renderer/container/resume/index.tsx
+++ app/renderer/container/resume/index.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import './index.less';
 import SparkeInput from '@/src/common/components/SparkeInput';
+import ImageUpload from '@/src/common/components/SparkeUpload/ImageUpload';
 
 function Resume() {
   return (
     <div>
       <SparkeInput value={2} />
+      <ImageUpload />
     </div>
   );
 }
